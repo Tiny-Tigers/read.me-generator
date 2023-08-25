ff99d669a7a0515da1ccbf05233f975ecf9074e2
Andrew Erickson <andrewerickson@Andrews-iMac.local>
2023-08-24 19:58:21 -0500

    initial

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,35 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the intended funcion of the project'
+    },
 
+    // Installation, 
+    // Usage, 
+    // License, 
+    // Contributing, 
+    // Tests,
+    // Questions
+    
+];
+
+inquirer.prompt(questions).then((answers) => {
+         console.log(answers)
+})
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, answers) {[
+    fileName = 'README.md',
+]}
+
+//fs.writeFile(fileName, JSON.stringify(answers))
 
 // TODO: Create a function to initialize app
 function init() {}
